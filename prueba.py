97fee00f029bf95e505214e963d3af8fe222aa96
RAFAEL LUCENA MARTINEZ <>
2017-11-06 09:14:51 +0100

    Archivo con la explicación POO

--- prueba.py
+++ prueba.py
@@ -7,7 +7,12 @@ import numpy as np
 dato = int(input("Introduce un número:  "))
 print(type(dato))
 
+"""Esto es un comentario 
+que vamos a utilizar
+ para una prueba"""
 
 
 
+for i in range(5):
+    print ( 'La variable es',i)
 
